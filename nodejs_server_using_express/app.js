5e0bf7f18f33c91a6a7e9ac46f4628e4a2c83787
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-07-28 01:58:49 -0700

    add get and post method

--- nodejs_server_using_express/app.js
+++ nodejs_server_using_express/app.js
@@ -1,16 +1,41 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+import Express from 'express';
+import Menu from "./menu.js";
+
+const app = Express();
+const port = 3000;
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }))
+
+const mid = (req, res, next) => {
+  console.log(req.query);
+  console.log(req.params);
+  next();
+}
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// });
+
+app.get('/menu/:id', mid, (req, res) => {
+  // res.send('Welcome To Our Cafe!');
+  res.json(Menu.find((menu) => {
+    return + req.params.id === menu.id
+  }));
+  res.send(req.params.id);
+
+})
+
+app.post("/add", (req, res) => {
+  console.log(req.body.id);
+  res.sendStatus(200);
+})
 
 app.post('/hello', (req, res) => {
   res.send("You just called the post method at ' / hello'!\n")
 });
 
-app.listen(port, () => {
-  console.log(`The server is listening from port ${port}`);
-})
+app.listen(port, () =>
+  console.log(`The server is listening from port ${port}`)
+)
---
+++ nodejs_server_using_express/menu.js
@@ -0,0 +1,16 @@
+const Menu = [
+  {
+    id: 1,
+    name: "Coffee",
+  },
+  {
+    id: 2,
+    name: "Hot Chocolate",
+  },
+  {
+    id: 3,
+    name: "Orange Juice",
+  }
+]
+
+export default Menu;
