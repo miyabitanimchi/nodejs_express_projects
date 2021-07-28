3eb8604ed867a7433cd4c003a1ed407b566ce644
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-07-28 16:59:40 -0700

    add static

--- nodejs_server_using_express/app.js
+++ nodejs_server_using_express/app.js
@@ -1,12 +1,16 @@
 import Express from 'express';
 import Menu from "./menu.js";
 import fs from 'fs';
+import ejs from 'ejs';
+
 
 const app = Express();
 const port = 3000;
 app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }))
-console.log(app);
+app.use(Express.urlencoded({ extended: true }));
+// STATIC FILE
+app.use(Express.static('public'));
+app.set('ejs', ejs.renderFile);
 
 const mid = (req, res, next) => {
   console.log(req.query);
@@ -14,8 +18,9 @@ const mid = (req, res, next) => {
   next();
 }
 
+// GET
 app.get('/htmlfile', (req, res) => {
-  fs.readFile("fstest.html", (err, data) => {
+  fs.readFile("index.html", (err, data) => {
     if (err) console.log(err);
     res.writeHead(200, { 'Content-type': 'text/html' });
     res.write(data);
@@ -31,6 +36,7 @@ app.get('/menu/:id', mid, (req, res) => {
   res.send(req.params.id);
 })
 
+// POST
 app.post("/add", (req, res) => {
   console.log(req.body.id);
   res.sendStatus(200);
@@ -40,6 +46,25 @@ app.post('/hello', (req, res) => {
   res.send("You just called the post method at ' / hello'!\n")
 });
 
+// PUT
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+});
+
+// DELETE
+app.delete('/deleteuser', function (req, res) {
+  res.send('Got a DELETE request at /user')
+});
+
+// STATIC
+app.get('/static', (req, res) => {
+  res.render('static.ejs', {
+    title: 'Hello World!',
+    content: 'Lorem Ipsum yay yay yay!'
+  });
+});
+
 app.listen(port, () =>
   console.log(`The server is listening from port ${port}`)
-)
+);
+
