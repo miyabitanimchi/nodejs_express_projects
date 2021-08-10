42e9336d2f1c60139bd3036573e3fcddd7562c2d
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-09 23:19:11 -0700

    add functions on cli app

--- cli_app/src/app.js
+++ cli_app/src/app.js
@@ -1,4 +1,6 @@
 const process = require('process');
+const os = require('os');
+const hddserial = require('hddserial');
 
 // export const cli = (args) => {
 //   console.log(args);
@@ -39,8 +41,47 @@ const calculator = (num1, num2, operator) => {
   }
 }
 
+// show architecture
+const showArch = (arg) => {
+  if (arg === "-arch") {
+    console.log(`This processor architecture is ${process.arch}`);
+  }
+}
+
+// show CPU
+const showCPU = (arg) => {
+  if (arg === "-cpu") console.log(os.cpus());
+}
+
+// show RAM (Random Access Memory)
+const showRAM = (arg) => {
+  if (arg === "-ram") console.log(process.memoryUsage());
+}
+
+// show HDD
+const showHDD = (arg) => {
+  if (arg === "-hdd") hddserial.all((err, serial) => console.log(`HDD serial serial is ${serial}`))
+}
+
+// show hostname
+const showHostname = (arg) => {
+  if (arg === "-hostname") console.log(os.hostname());
+}
+
+// show ip address
+const showIPAddress = (arg) => {
+  if (arg === "-ipaddress") console.log(os.networkInterfaces().en0[3].address);
+}
+
+// showArch(process.argv[2]);
+// showCPU(process.argv[2]);
+// showRAM(process.argv[2]);
+showHDD(process.argv[2]);
+showHostname(process.argv[2]);
+showIPAddress(process.argv[2]);
 test(process.argv[2]);
-calculator(process.argv[2], process.argv[3], process.argv[4]);
+
+// calculator(process.argv[2], process.argv[3], process.argv[4]);
 
 // How to launch 
 // In the src folder,
