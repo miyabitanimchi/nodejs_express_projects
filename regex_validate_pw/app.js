752318b2a7a11007011cbc93a19d35ce1140cc61
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-11 00:29:10 -0700

    change code for regex

--- regex_validate_pw/app.js
+++ regex_validate_pw/app.js
@@ -1,12 +1,22 @@
 const validatePassword = (password) => {
-  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*](?=.*[0-9]{6,})$/;
+  const regex = /(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])/;
 
-  if (password.match(regex)) {
-    console.log("valid password");
+  const passwordArr = password.split("");
+  // check if there are 6 or more numbers
+  const numOfDigits = passwordArr.filter((str) => str.match(/[0-9]/));
+
+  if (numOfDigits.length > 5) {
+    if (password.match(regex)) {
+      console.log("valid password");
+    } else {
+      console.log("invalid password");
+    }
   } else {
-
     console.log("invalid password");
   }
-}
+};
 
-console.log(validatePassword("Aa@3331333"));
+console.log(validatePassword("0A2892a!9"));
+console.log(validatePassword("qweasd"));
+console.log(validatePassword("P12a8p%"));
+console.log(validatePassword("*s55t4V289"));
