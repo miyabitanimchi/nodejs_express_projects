16bb04fee89bf7cdf2ecebda417c1d578afbee60
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-10 18:48:54 -0700

    add disk space etc

--- express_app/app.js
+++ express_app/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const diskSpace = require('check-disk-space').default;
 
 const process = require('process');
 const os = require('os');
 
-app.get('/arch', (req, res) => res.send(`This computer architecture is ${process.arch}`));
-app.get('/cpu', (req, res) => res.send(`Model : ${os.cpus()[0].model}`));
-app.get('/ram', (req, res) => res.send(process.memoryUsage()));
-app.get('/hostname', (req, res) => res.send(`This computer's hostname is ${os.hostname()}`));
-app.get('/ip', (req, res) => res.send(`This computer's IP address is ${os.networkInterfaces().en0[3].address}`));
+const showDiskspace = () => {
+
+}
+
+app.get('/api/arch', (req, res) => res.send(`This computer architecture is ${process.arch}`));
+app.get('/api/cpu', (req, res) => res.send(`Model : ${os.cpus()[0].model}`));
+app.get('/api/ram', (req, res) => res.send(`${String(os.totalmem() / (1024 ** 3))} GB`));
+app.get('/api/diskspace', (req, res) => diskSpace('/Users/miyabi/').then((diskSpace) => {
+  res.send(`The total memory is ${String(Math.round(diskSpace.size / (1024 ** 3)))} GB, and the free memory is ${String(Math.round(diskSpace.free / (1024 ** 3)))} GB.`);
+}));
+app.get('/api/hostname', (req, res) => res.send(`This computer's hostname is ${os.hostname()}`));
+app.get('/api/ip', (req, res) => res.send(`This computer's IP address is ${os.networkInterfaces().en0[3].address}`));
 
 app.listen(port, () => console.log(`The server is listening from port ${port}`));
