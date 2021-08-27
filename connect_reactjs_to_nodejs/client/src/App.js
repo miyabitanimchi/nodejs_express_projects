b95c5fb47905962ac9af0f715a84ef79ddfa442a
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-08-26 23:54:20 -0700

    enhance the app

--- connect_reactjs_to_nodejs/client/src/App.js
+++ connect_reactjs_to_nodejs/client/src/App.js
@@ -1,39 +1,66 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 function App() {
   const [name, setName] = useState("");
-  const [home, setHome] = useState("");
+  const [country, setCountry] = useState("");
+  const [studentsData, setstudentsData] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:4000/home").then((res) => {
-      setHome(res.data);
+      setstudentsData(res.data);
+      console.log(res.data);
     });
-  }, []);
+  }, [name, country]);
 
-  const postName = async (e) => {
+  const postNewStudentdata = async (e) => {
     e.preventDefault();
+    // setName("");
+    // setCountry("");
 
     try {
-      await axios.post("http://localhost:4000/post-name", {
-        name,
+      await axios.post("http://localhost:4000/post-new-student-data", {
+        name: name,
+        country: country,
       });
+      // setName(""); // ?
+      // setCountry(""); // ?
     } catch (err) {
       console.log(err);
     }
+
+    console.log("worked");
+    setName("");
+    setCountry("");
   };
   return (
     <div className="App">
-      <form onSubmit={postName}>
+      <form onSubmit={postNewStudentdata}>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+        <input
+          type="text"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
         <button type="submit">Send Name</button>
       </form>
-      {home}
+      <div>
+        {studentsData.length !== 0 &&
+          studentsData.map((student, index) => {
+            return (
+              <div key={index}>
+                <p>
+                  Name: {student.name} / Country: {student.country}
+                </p>
+                <p></p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- connect_reactjs_to_nodejs/server/server.js
+++ connect_reactjs_to_nodejs/server/server.js
@@ -1,25 +1,55 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 4000;
 const cors = require("cors");
+const fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 app.get("/", cors(), async (req, res) => {
-  res.send("this is working!");
+  res.send("working!");
 });
 
-app.post("/post-name", async (req, res) => {
-  let { name } = req.body;
-  console.log(name);
+app.post("/post-new-student-data", async (req, res) => {
+  let newStudentData = req.body;
+
+  fs.readFile(
+    path.join(__dirname, "data", "students.json"),
+    (err, currentData) => {
+      if (err) throw err;
+      const updatedStudentsData = JSON.parse(currentData);
+      updatedStudentsData.push(newStudentData);
+      fs.writeFileSync(
+        path.join(__dirname, "data", "students.json"),
+        JSON.stringify(updatedStudentsData),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
+  );
+  // res.send(
+  //   `new student data added! Name: ${newStudentData.name} Country: ${newStudentData.country}`
+  // );
+  console.log(
+    `new student data added! Name: ${newStudentData.name} Country: ${newStudentData.country}`
+  );
 });
 
 app.get("/home", cors(), async (req, res) => {
-  res.send("this is the data from node.js!");
+  const studentsData = fs.readFileSync(
+    path.join(__dirname, "data", "students.json"),
+    (err, data) => {
+      if (err) throw err;
+      return JSON.parse(data);
+    }
+  );
+  res.send(studentsData);
 });
 
 app.listen(port, () => {
-  console.log(`The server is listening at http://localhost:${port}`);
+  console.log(`The server is listening at http://localhost:${port} (-ω-) ~ ☀️`);
 });
